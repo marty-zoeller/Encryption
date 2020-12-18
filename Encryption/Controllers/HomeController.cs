dc7a733a1aaaa0cd07b304d637761f8971781ce8
Marty Goetz <MartyG@zoeller.com>
2020-12-18 14:16:20 -0500

    xxxx

--- Encryption/Controllers/HomeController.cs
+++ Encryption/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Encryption.Models;
 using Zoeller;
-using Microsoft.AspNetCore.Authorization;
 
 namespace Encryption.Controllers
 {
